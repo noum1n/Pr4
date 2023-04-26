6fd1a713dfff63bb41f9ad7822459c7f41ec59fa
MiYuri <alli13let@gmail.com>
2023-04-26 21:00:49 +0400

    Added class TPen

---
+++ ColorsFromFile.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class TPen {
+public:
+	int Fcolor[3];
+
+	void set(int r, int g, int b) {
+		Fcolor[0] = r;
+		Fcolor[1] = g;
+		Fcolor[2] = b;
+	}
+
+
+
+int main() {
+
+
+	return 0;
+}
+
