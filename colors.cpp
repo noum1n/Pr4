7691fcb75cf49b200ddc346e7b909622a9eec172
MiYuri <alli13let@gmail.com>
2023-04-26 20:58:17 +0400

    Added to Pr6 repozitory, my bad

---
+++ colors.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class TPen {
+public:
+	std::string Fcolor;
+	TPen(std::string color) {
+		Fcolor = color;
+	};
+};
+
+	int main() {
+		TPen pen("FFFFFF");
+		std::cout <<" Colour = "<< pen.Fcolor<<std::endl;
+	
+	return 0;
+}
